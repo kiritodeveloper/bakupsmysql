68eed8095a6f7dfe639463fd776b0729af4cfbc4
Administrator <admin@example.com>
2019-05-14 15:43:47 -0400

    xammp

---
+++ xammp_bakup.bat
@@ -0,0 +1,30 @@
+@echo off
+
+title sacar backup  mysql
+color 9F
+
+@echo off
+cd C:\xampp\mysql\bin
+@echo ----------------------------------------------
+@echo PREPARANDO EL ENTORNO ...........................
+@echo ----------------------------------------------
+@echo CREANDO CARPETA.........
+IF exist bakup ( echo bakup exists )ELSE ( mkdir bakup && echo bakup created)
+@pause
+@echo ----------------------------------------------
+@echo INTRODUSCA EL NOMBRE DE BASE DE DATOS ...........................
+@echo ----------------------------------------------
+
+set/p nombre= ..:cual es el nombre de la base de datos?:..
+cls
+@echo ----------------------------------------------
+@echo INTRODUSCA EL NOMBRE DEL BAKUP ...........................
+@echo ----------------------------------------------
+set/p bakup= ..:COmo kieres guardarlo?:..
+cls
+mysqldump -u root -p %nombre% > C:\xampp\mysql\bin\bakup\%bakup%.sql
+@echo ----------------------------------------------
+@echo ABRIENDO CARPETA ...........................
+@echo ----------------------------------------------
+start %windir%\explorer.exe C:\xampp\mysql\bin\bakup\
+@pause
