d21c5831f2179a6163e64a88035caf5f4bf69874
Administrator <admin@example.com>
2019-05-15 15:07:12 -0400

    c#

--- copias segurity msql/copias segurity msql/Form1.Designer.cs
+++ copias segurity msql/copias segurity msql/Form1.Designer.cs
@@ -29,27 +29,71 @@
         private void InitializeComponent()
         {
             this.sideNav1 = new DevComponents.DotNetBar.Controls.SideNav();
+            this.sideNavPanel1 = new DevComponents.DotNetBar.Controls.SideNavPanel();
+            this.buttonX2 = new DevComponents.DotNetBar.ButtonX();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
             this.sideNavItem1 = new DevComponents.DotNetBar.Controls.SideNavItem();
             this.separator1 = new DevComponents.DotNetBar.Separator();
             this.sideNavItem2 = new DevComponents.DotNetBar.Controls.SideNavItem();
-            this.sideNavPanel1 = new DevComponents.DotNetBar.Controls.SideNavPanel();
+            this.sideNavItem3 = new DevComponents.DotNetBar.Controls.SideNavItem();
+            this.buttonX3 = new DevComponents.DotNetBar.ButtonX();
             this.sideNav1.SuspendLayout();
+            this.sideNavPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // sideNav1
             // 
+            this.sideNav1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.sideNav1.Controls.Add(this.sideNavPanel1);
             this.sideNav1.Dock = System.Windows.Forms.DockStyle.Left;
             this.sideNav1.Items.AddRange(new DevComponents.DotNetBar.BaseItem[] {
             this.sideNavItem1,
             this.separator1,
-            this.sideNavItem2});
+            this.sideNavItem2,
+            this.sideNavItem3});
             this.sideNav1.Location = new System.Drawing.Point(5, 1);
             this.sideNav1.Name = "sideNav1";
             this.sideNav1.Padding = new System.Windows.Forms.Padding(1);
             this.sideNav1.Size = new System.Drawing.Size(959, 395);
             this.sideNav1.TabIndex = 0;
             this.sideNav1.Text = "sideNav1";
+            this.sideNav1.Click += new System.EventHandler(this.sideNav1_Click);
+            // 
+            // sideNavPanel1
+            // 
+            this.sideNavPanel1.Controls.Add(this.buttonX3);
+            this.sideNavPanel1.Controls.Add(this.buttonX2);
+            this.sideNavPanel1.Controls.Add(this.buttonX1);
+            this.sideNavPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sideNavPanel1.Location = new System.Drawing.Point(94, 37);
+            this.sideNavPanel1.Name = "sideNavPanel1";
+            this.sideNavPanel1.Size = new System.Drawing.Size(860, 357);
+            this.sideNavPanel1.TabIndex = 2;
+            this.sideNavPanel1.Visible = false;
+            // 
+            // buttonX2
+            // 
+            this.buttonX2.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX2.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX2.Location = new System.Drawing.Point(327, 144);
+            this.buttonX2.Name = "buttonX2";
+            this.buttonX2.Size = new System.Drawing.Size(128, 37);
+            this.buttonX2.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX2.TabIndex = 1;
+            this.buttonX2.Text = "RESTAURAR";
+            this.buttonX2.Click += new System.EventHandler(this.buttonX2_Click);
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Location = new System.Drawing.Point(138, 144);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(127, 39);
+            this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX1.TabIndex = 0;
+            this.buttonX1.Text = "SACAR BAKUP";
+            this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
             // 
             // sideNavItem1
             // 
@@ -75,14 +119,28 @@
             this.sideNavItem2.Panel = this.sideNavPanel1;
             this.sideNavItem2.Symbol = "";
             this.sideNavItem2.Text = "Home";
+            this.sideNavItem2.Click += new System.EventHandler(this.sideNavItem2_Click);
             // 
-            // sideNavPanel1
+            // sideNavItem3
             // 
-            this.sideNavPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.sideNavPanel1.Location = new System.Drawing.Point(77, 37);
-            this.sideNavPanel1.Name = "sideNavPanel1";
-            this.sideNavPanel1.Size = new System.Drawing.Size(877, 357);
-            this.sideNavPanel1.TabIndex = 2;
+            this.sideNavItem3.Name = "sideNavItem3";
+            this.sideNavItem3.Panel = this.sideNavPanel1;
+            this.sideNavItem3.Symbol = "";
+            this.sideNavItem3.SymbolColor = System.Drawing.Color.Gray;
+            this.sideNavItem3.Text = "Opciones";
+            this.sideNavItem3.Click += new System.EventHandler(this.sideNavItem3_Click);
+            // 
+            // buttonX3
+            // 
+            this.buttonX3.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX3.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX3.Location = new System.Drawing.Point(528, 144);
+            this.buttonX3.Name = "buttonX3";
+            this.buttonX3.Size = new System.Drawing.Size(136, 37);
+            this.buttonX3.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX3.TabIndex = 2;
+            this.buttonX3.Text = "SALIR";
+            this.buttonX3.Click += new System.EventHandler(this.buttonX3_Click);
             // 
             // Form1
             // 
@@ -95,6 +153,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.sideNav1.ResumeLayout(false);
             this.sideNav1.PerformLayout();
+            this.sideNavPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -106,6 +165,10 @@
         private DevComponents.DotNetBar.Controls.SideNavItem sideNavItem1;
         private DevComponents.DotNetBar.Separator separator1;
         private DevComponents.DotNetBar.Controls.SideNavItem sideNavItem2;
+        private DevComponents.DotNetBar.Controls.SideNavItem sideNavItem3;
+        private DevComponents.DotNetBar.ButtonX buttonX2;
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private DevComponents.DotNetBar.ButtonX buttonX3;
     }
 }
 
--- copias segurity msql/copias segurity msql/Form1.cs
+++ copias segurity msql/copias segurity msql/Form1.cs
@@ -9,18 +9,57 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevComponents.DotNetBar;
 using DevComponents;
+using System.Diagnostics;
+using System.IO;
 
 
 namespace copias_segurity_msql
 {
     public partial class Form1 : DevComponents.DotNetBar.RibbonForm
     {
+
         public Form1()
         {
+
             InitializeComponent();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sideNavItem3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sideNav1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonX1_Click(object sender, EventArgs e)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "C:\\Users\\kirito\\Desktop\\bakupsmysql\\xammp_bakup";
+            p.Start();
+        }
+
+        private void buttonX2_Click(object sender, EventArgs e)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "C:\\Users\\kirito\\Desktop\\bakupsmysql\\restaurar";
+            p.Start();
+        }
+
+        private void buttonX3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void sideNavItem2_Click(object sender, EventArgs e)
         {
 
         }
